1b2226607e13d8419d7515ef0350351dd0d8c11f
2eek <khwan9404@yu.ac.kr>
2024-01-11 18:01:07 +0900

    feat:[스프링 입문] 업로드

--- src/test/java/hello/hellospring/HelloSpringApplicationTests.java
+++ src/test/java/hello/hellospring/HelloSpringApplicationTests.java
@@ -1,11 +1,10 @@
 package hello.hellospring;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Scanner;
 
-@SpringBootTest
+
 class HelloSpringApplicationTests {
 
     @Test
